98e483fae16b72c5a78c383abedce1d4f510329c
yousri <eng.yousrii@gmail.com>
2021-04-20 06:54:26 +0200

    your message

---
+++ main.c
@@ -0,0 +1,18 @@
+#ifndef STRUCTURES_H_INCLUDED
+#define STRUCTURES_H_INCLUDED
+#define NULL 0
+
+struct student{
+    char name[40];
+    int id;
+    int d , m , y;
+    int last_score;
+};
+
+struct Node{
+    struct Node* next;
+    struct student data;
+};
+
+
+#endif // STRUCTURES_H_INCLUDED
